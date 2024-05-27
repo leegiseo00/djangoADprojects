51280a0fa6f398034cd2fd30dba9ca2b760eaebb
lee <k0207y2@gmail.com>
2024-05-27 11:45:24 +0900

    aa

--- common/views.py
+++ common/views.py
@@ -1,5 +1,9 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
 
 from .forms import UserForm
 
