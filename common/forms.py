124e3153b3fa2fd684a1ab82b48895928cef2961
lee <k0207y2@gmail.com>
2024-06-02 22:17:48 +0900

    ㅁㅁ

--- common/forms.py
+++ common/forms.py
@@ -1,6 +1,12 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from pybo.models import Profile
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['introduction']
 
 
 class UserForm(UserCreationForm):
